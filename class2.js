f68b38ef5b034a061032eebe7eb540f3843fa105
aiwasim <aiwasim.dev1@gmail.com>
2023-06-27 23:26:26 +0600

    class 2 done

---
+++ class2.js
@@ -0,0 +1,23 @@
+// const users =
+//     [
+//         { id: 101, name: 'Abul', job: 'doctor' }
+//     ]
+
+// console.log(users[0].name);
+
+
+const person =
+{
+    id: 101,
+    name: 'Wasim',
+    job: 'Web Developer',
+    location:
+    {
+        area: 'Mugda Para',
+        PS: 'Mugda',
+        PO: 'Basabo'
+    },
+    salary: 20500,
+}
+
+console.log(person.location1?.PO);
